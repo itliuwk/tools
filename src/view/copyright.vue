3923e83e1c66fb9a7e804dff35d1af3facd39035
liuwk <1834183435@qq.com>
2019-06-06 17:34:50 +0800

    增加 github链接

--- src/view/copyright.vue
+++ src/view/copyright.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <p class="Copyright"> Copyright©2019 随心it,随心工具,刘伟坤</p>
-    <!--<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");-->
-    <!--document.write(unescape("%3Cspan id='cnzz_stat_icon_1277669186'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s96.cnzz.com/z_stat.php%3Fid%3D1277669186%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>-->
+    <p class="Copyright"> Copyright©2019 随心it,随心工具,刘伟坤
+      <a target="_blank" href="https://github.com/itliuwk/tools">Github</a>
+    </p>
 
   </div>
 </template>
@@ -19,5 +19,8 @@
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-
+a{
+  text-decoration: none;
+  color: #188ae2;
+}
 </style>
